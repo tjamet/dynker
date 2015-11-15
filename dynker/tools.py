fcbd12b8faf659790fc441ce59b1e667555ce630
Thibault Jamet <thibault.jamet@gmail.com>
2015-11-15 20:36:52 +0100

    Use independant logger for git tools

--- dynker/tools.py
+++ dynker/tools.py
@@ -7,24 +7,25 @@ class GitMTime(object) :
         self.repo = git.Repo()
         self.hist = self.repo.iter_commits()
         self.MTimes = {}
+        self.logger = logging.getLogger(self.__class__.__name__)
     def isDirty(self, file) :
         for diff in git.Repo().head.commit.diff(None) :
             if diff.b_path == file or diff.a_path == file :
                 logging.debug("%s is dirty",file)
                 return True
-        logging.debug("%s is clean",file)
+        self.logger.debug("%r are clean",files)
         return False
 
     def getMTime(self, file) :
         file = os.path.normpath(file)
         if file in self.repo.untracked_files or self.isDirty(file) :
             mtime = os.lstat(file).st_mtime
-            logging.debug("%s is dirty/untracked, returning its system mtime: %f", file, mtime)
+            self.logger.debug("%s is dirty/untracked, returning its system mtime: %f", file, mtime)
             return mtime
 
         try :
             mtime = self.MTimes[file]
-            logging.debug("%s has been last modified in git at %d",file, mtime)
+            self.logger.debug("%s has been last modified in git at %d",file, mtime)
             return mtime
         except KeyError :
             pass
@@ -34,12 +35,12 @@ class GitMTime(object) :
                 self.MTimes[f] = commit.committed_date
             try :
                 mtime = self.MTimes[file]
-                logging.debug("%s has been last modified in git at %d",file, mtime)
+                self.logger.debug("%s has been last modified in git at %d",file, mtime)
                 return mtime
             except KeyError :
                 pass
         mtime = os.lstat(file).st_mtime
-        logging.debug("%s is not under scm, returning its system mtime: %f", file, mtime)
+        self.logger.debug("%s is not under scm, returning its system mtime: %f", file, mtime)
         return mtime
 
     @classmethod
