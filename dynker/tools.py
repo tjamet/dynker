70e0901fcb9a93d344044b5af9aebe0f70bc1a3c
Thibault Jamet <thibault.jamet@gmail.com>
2015-11-15 20:44:12 +0100

    Add last commit retrieve feature to git tools

--- dynker/tools.py
+++ dynker/tools.py
@@ -18,6 +18,25 @@ class GitMTime(object) :
         self.logger.debug("%r are clean",files)
         return False
 
+    def getLastCommit(self, *files, **kwds) :
+        files = list(files) + kwds.get('files',[])
+        strict = kwds.get('strict', True)
+        files = set(map(os.path.normpath,files))
+        self.logger.debug("Getting latest commit on set of files: %r"%files)
+        for commit in self.repo.iter_commits() :
+            if strict :
+                if files & set(commit.stats.files.keys()) :
+                    return commit.hexsha
+            else :
+                for f in commit.stats.files.keys() :
+                    for f2 in files :
+                        if f.startswith(f2) :
+                            self.logger.debug("commit %s changed file %s, matching our requirement %s", commit.hexsha, f, f2)
+                            return commit.hexsha
+                        else :
+                            self.logger.debug("commit %s changed file %s, but not in our requirement %s", commit.hexsha, f, f2)
+        return None
+
     def getMTime(self, file) :
         file = os.path.normpath(file)
         if file in self.repo.untracked_files or self.isDirty(file) :
