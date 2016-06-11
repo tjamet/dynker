30d641e70ac27af987ad972862831adfddced1c5
Thibault Jamet <thibault.jamet@gmail.com>
2016-06-12 00:33:34 +0200

    update line filter to use sure assertions

--- tests/unit/test_filters_line.py
+++ tests/unit/test_filters_line.py
@@ -2,61 +2,65 @@ import unittest
 import sure
 import dynker.filters.line
 
+def factoryFilter(c, *args, **kwds):
+    class TestFilter(c):
+        def filter(self, *args, **kwds):
+            r = c.filter(self, *args, **kwds)
+            if isinstance(r, (str, unicode)):
+                return r
+            if isinstance(r, (list, tuple)):
+                return r
+            return list(r)
+    return TestFilter(*args, **kwds)
+
 class TestLineFilter(unittest.TestCase):
 
   def test_noFilter(self):
-    self.assertEqual(dynker.filters.line.LineFilter().filter("Hello World"),"Hello World")
-    self.assertEqual(list(dynker.filters.line.LineFilter().filter(["Hello"])), ["Hello"])
-    self.assertEqual(dynker.filters.line.LineFilter().filter("Hello \nWorld"),"Hello \nWorld")
-    self.assertEqual(list(dynker.filters.line.LineFilter().filter(["Hello", "World"])), ["Hello", "World"])
+    filter = factoryFilter(dynker.filters.line.LineFilter)
+    filter.filter.when.called_with("Hello World").should.return_value(["Hello World"])
+    filter.filter.when.called_with("Hello World", restore_type=True).should.return_value("Hello World")
+    filter.filter.when.called_with(["Hello \nWorld"]).should.return_value(["Hello \nWorld"])
+    filter.filter.when.called_with("Hello \nWorld", restore_type=True).should.return_value("Hello \nWorld")
+    filter.filter.when.called_with("Hello \nWorld").should.return_value(["Hello ", "World"])
+    filter.filter.when.called_with(["Hello", "World"]).should.return_value(["Hello", "World"])
+    filter.filter.when.called_with(["Hello", None, "World"]).should.return_value(["Hello", "World"])
     
   def test_PatternFilterRe(self):
-    filter = dynker.filters.line.LineFilter()
+    filter = factoryFilter(dynker.filters.line.LineFilter)
     filter.withFilter(dynker.filters.MatchingLineFilter("^World$"))
-    self.assertEqual(filter.filter("Hello World"),"")
-    self.assertEqual(list(filter.filter(["Hello"])), [])
-    self.assertEqual(filter.filter("Hello \nWorld"),"World")
-    self.assertEqual(list(filter.filter(["Hello", "World"])), ["World"])
+    filter.filter.when.called_with("Hello World").should.return_value([])
+    filter.filter.when.called_with(["Hello", "World"]).should.return_value(["World"])
     
-    filter = dynker.filters.line.LineFilter()
+    filter = factoryFilter(dynker.filters.line.LineFilter)
     filter.withFilter(dynker.filters.NotMatchingLineFilter("^World$"))
-    self.assertEqual(filter.filter("Hello World"),"Hello World")
-    self.assertEqual(list(filter.filter(["Hello"])), ["Hello"])
-    self.assertEqual(filter.filter("Hello \nWorld"),"Hello ")
-    self.assertEqual(list(filter.filter(["Hello", "World"])), ["Hello"])
+    filter.filter.when.called_with("Hello World").should.return_value(["Hello World"])
+    filter.filter.when.called_with(["Hello", "World"]).should.return_value(["Hello"])
     
   def test_ReplaceLineRe(self):
-    filter = dynker.filters.line.LineFilter()
+    filter = factoryFilter(dynker.filters.line.LineFilter)
     filter.withFilter(dynker.filters.line.ReplaceLineReFilter("orld"))
-    self.assertEqual(filter.filter("Hello World"),"Hello W")
-    self.assertEqual(list(filter.filter(["Hello"])), ["Hello"])
-    self.assertEqual(filter.filter("Hello \nWorld"),"Hello \nW")
-    self.assertEqual(list(filter.filter(["Hello", "World"])), ["Hello", "W"])
+    filter.filter.when.called_with("Hello World").should.return_value(["Hello W"])
+    filter.filter.when.called_with("Hello Wor").should.return_value(["Hello Wor"])
+    filter.filter.when.called_with(["Hello", "World"]).should.return_value(["Hello", "W"])
 
-    filter = dynker.filters.line.LineFilter()
+    filter = factoryFilter(dynker.filters.line.LineFilter)
     filter.withFilter(dynker.filters.line.ReplaceLineReFilter("o(rl)d", sub=lambda match:match.group(1)))
-    self.assertEqual(filter.filter("Hello World"),"Hello Wrl")
-    self.assertEqual(list(filter.filter(["Hello"])), ["Hello"])
-    self.assertEqual(filter.filter("Hello \nWorld"),"Hello \nWrl")
-    self.assertEqual(list(filter.filter(["Hello", "World"])), ["Hello", "Wrl"])
+    filter.filter.when.called_with(['Hello', 'World']).should.return_value(['Hello', 'Wrl'])
 
-    filter = dynker.filters.line.LineFilter()
+    filter = factoryFilter(dynker.filters.line.LineFilter)
     filter.withFilter(dynker.filters.line.ReplaceLineReFilter("orld", sub="repl"))
-    self.assertEqual(filter.filter("Hello World"),"Hello Wrepl")
-    self.assertEqual(list(filter.filter(["Hello"])), ["Hello"])
-    self.assertEqual(filter.filter("Hello \nWorld"),"Hello \nWrepl")
-    self.assertEqual(list(filter.filter(["Hello", "World"])), ["Hello", "Wrepl"])
+    filter.filter.when.called_with(['Hello', 'World']).should.return_value(['Hello', 'Wrepl'])
 
   def test_ReplaceLineRePrio(self):
-    filter = dynker.filters.line.LineFilter()
+    filter = factoryFilter(dynker.filters.line.LineFilter)
     for f in dynker.filters.line.ReplaceLineReFilter("orl", prio = 1), dynker.filters.line.ReplaceLineReFilter("Wd", sub="ok") :
         filter.withFilter(f)
-    self.assertEqual(filter.filter("Hello World"), "Hello ok")
+    filter.filter.when.called_with("Hello World").should.return_value(["Hello ok"])
 
-    filter = dynker.filters.line.LineFilter()
+    filter = factoryFilter(dynker.filters.line.LineFilter)
     for f in dynker.filters.line.ReplaceLineReFilter("orl", prio = 1), dynker.filters.line.ReplaceLineReFilter("Wd", sub="ok", prio=3) :
         filter.withFilter(f)
-    self.assertEqual(filter.filter("Hello World"), "Hello Wd")
+    filter.filter.when.called_with("Hello World").should.return_value(["Hello Wd"])
 
 class TestStripLinesFilter(unittest.TestCase):
     def test_filter_lines_are_stripped(self):
