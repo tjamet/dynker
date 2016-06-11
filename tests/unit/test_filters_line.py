3e0b95328a5e27b7942668e73da01d0a8d65effa
Thibault Jamet <thibault.jamet@gmail.com>
2016-06-11 22:46:45 +0200

    Add strip lines filter test

--- tests/unit/test_filters_line.py
+++ tests/unit/test_filters_line.py
@@ -1,4 +1,5 @@
 import unittest
+import sure
 import dynker.filters.line
 
 class TestLineFilter(unittest.TestCase):
@@ -56,6 +57,17 @@ class TestLineFilter(unittest.TestCase):
     for f in dynker.filters.line.ReplaceLineReFilter("orl", prio = 1), dynker.filters.line.ReplaceLineReFilter("Wd", sub="ok", prio=3) :
         filter.withFilter(f)
     self.assertEqual(filter.filter("Hello World"), "Hello Wd")
+
+class TestStripLinesFilter(unittest.TestCase):
+    def test_filter_lines_are_stripped(self):
+        filter = dynker.filters.line.StripLinesFilter()
+        list(filter.filter([
+            " a line with trailing whitespaces should be stripped        ",
+            "\t a line with trailing tabs should be stripped  \t\t   ",
+        ])).should.be.eql([
+            "a line with trailing whitespaces should be stripped",
+            "a line with trailing tabs should be stripped",
+        ])
 
 if __name__ == '__main__':
     unittest.main()
